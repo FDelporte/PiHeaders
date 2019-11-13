80a331243adae86a28e5a68e7f2271a616fa72e3
Frank Delporte <f.delporte@televic.com>
2019-11-13 23:52:26 +0100

    0.0.3 with correct module exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module be.webtechie.piheaders {
     exports be.webtechie.piheaders;
+    exports be.webtechie.piheaders.definition;
+    exports be.webtechie.piheaders.pin;
+    exports be.webtechie.piheaders.util;
 }
